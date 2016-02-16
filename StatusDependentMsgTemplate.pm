52ef484d28a171cc4078d672341b17c305552f8b
Stefano Bocconi <stefano@waag.org>
2016-02-16 13:04:42 +0100

    Added second task for not changing owner status

--- StatusDependentMsgTemplate.pm
+++ StatusDependentMsgTemplate.pm
@@ -40,13 +40,13 @@ You can also look up instructions at {RT->Config->Get('WebURL')}Articles/Article
     $instructions_message = "Dir. Legal / NB (jij!) maakt Memo en bijbehorende stukken (oa begroting) compleet\n";
     $group->LoadUserDefinedGroup('Finance');
     $nextstate = "memoCompleted";
-    $secondarygroup = RT::Group->new($RT::SystemUser);
-    $secondarygroup->LoadUserDefinedGroup('ProjectInrichtingBoink');
 
   } elsif ($status eq "memoCompleted" ){
     $instructions_message = "Dir. Legal / NB (jij!) geeft Finance opdracht om Achievo en Accountview te regelen\n";
     $group->LoadUserDefinedGroup('Finance');
     $nextstate = "accountReady";
+    $secondarygroup = RT::Group->new($RT::SystemUser);
+    $secondarygroup->LoadUserDefinedGroup('ProjectInrichtingBoink');
 
   } elsif ($status eq "accountReady" ){
     $instructions_message = "Finance MW (jij!) leegt Projectkaart ter goedkeuring aan PM voor\n";
@@ -76,12 +76,12 @@ You can also look up instructions at {RT->Config->Get('WebURL')}Articles/Article
   }
 
 
-  if (defined $nextstate){
-    $instructions_message .= "\nAls dit klaar is, dan aub de tiket naar status \"" . $nextstate . "\" zetten\n";
-  }
+  if ($status eq "memoCompleted"){
+    if (defined $nextstate){
+      $instructions_message .= "\nAls dit klaar is, dan aub de tiket naar status \"" . $nextstate . "\" zetten\n";
+    }
 
-  if (defined $group){
-    $instructions_message .= "De owner zal automatish veranderen naar de volgende user (naar de eerste in geval van meerdere users):\n\n";
+    $instructions_message .= "De owner zal de volgende user (of de eerste in geval van meerdere users) zijn (waarschijndelijk dat ben jij):\n\n";
 
     my $users = $group->UserMembersObj();
 
@@ -89,20 +89,66 @@ You can also look up instructions at {RT->Config->Get('WebURL')}Articles/Article
       $instructions_message .= $user->Name . "\n";
     }
 
-    $instructions_message .= "Als de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten nadat de status is veranderd.\n";
-  }
+    $instructions_message .= "\nAls de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten nadat de status is veranderd.\n";
 
-  if ( defined $secondarygroup){
-    $instructions_message .= "Er is ook een nieuwe ticket aangemaakt voor de inrichting van de projectmap\n";
-    $instructions_message .= "De owner is de volgende user (de eerste in geval van meerdere users):\n\n";
+    $instructions_message .= "\nAls jij de volgende owner bent, dan aub ook de volgende doen:\n\n";
 
-    my $users = $secondarygroup->UserMembersObj();
+    $instructions_message .= "Finance MW (jij!) leegt Projectkaart ter goedkeuring aan PM voor\n";
+    $group->LoadUserDefinedGroup('ProjectManagers');
+    $nextstate = "cardSent";
+
+    $instructions_message .= "\nAls dit klaar is, dan aub de tiket naar status \"" . $nextstate . "\" zetten\n";
+    
+    $instructions_message .= "De owner zal automatish veranderen naar de volgende user (naar de eerste in geval van meerdere users):\n\n";
+
+    $users = $group->UserMembersObj();
 
     while ( my $user = $users->Next ) {
       $instructions_message .= $user->Name . "\n";
     }
-    $instructions_message .= "Als de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten.\n";
+
+    $instructions_message .= "\nAls de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten nadat de status is veranderd.\n";
+
+    if ( defined $secondarygroup){
+      $instructions_message .= "\nEr is ook een nieuwe ticket aangemaakt voor de inrichting van de projectmap:\n\n";
+
+      $instructions_message .= RT::Config->Get('WebURL') . "/Search/Results.html?Query=DependedOnBy%20%3D%20" . $Ticket->id . "\n";
+
+      #my $DepOnBy = $Ticket->DependedOnBy;
+      #my $links = RT::Links->new($RT::SystemUser);
+      #$links->LimitReferredToBy($Ticket->id);
+      #while( my $l = $links->Next ) {
+      #  $instructions_message .= $l->id;
+      #}
+
+      $instructions_message .= "\nDe owner is de volgende user (de eerste in geval van meerdere users):\n\n";
+
+      my $users = $secondarygroup->UserMembersObj();
+
+      while ( my $user = $users->Next ) {
+        $instructions_message .= $user->Name . "\n";
+      }
+      $instructions_message .= "\nAls de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten.\n";
+    }
+  }else{
+
+    if (defined $nextstate){
+      $instructions_message .= "\nAls dit klaar is, dan aub de tiket naar status \"" . $nextstate . "\" zetten\n";
+    }
+
+    if (defined $group){
+      $instructions_message .= "De owner zal automatish veranderen naar de volgende user (naar de eerste in geval van meerdere users):\n\n";
+
+      my $users = $group->UserMembersObj();
+
+      while ( my $user = $users->Next ) {
+        $instructions_message .= $user->Name . "\n";
+      }
+
+      $instructions_message .= "\nAls de owner niet de eerste van de lijst moet zijn, dan aub de juste owner handmatig zetten nadat de status is veranderd.\n";
+    }
   }
+
   #$group->MemberEmailAddressesAsString();
   $instructions_message;
 }
